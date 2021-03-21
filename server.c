a7d69d621c57567477727e20e803aaeca1cd2b0e
EminUmutGercek <umutgercek1999@gmail.com>
2021-03-22 01:54:28 +0300

    Add basic structure of server.c
    Server is not even multiprocess yet.
    Plans for implementing concurrent functionality.
    1. simple fork()
    2. simple pthread
    3. thread pool

---
+++ server.c
@@ -0,0 +1,73 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BACKLOG 20
+
+int main(int argc, char *argv[]) {
+  struct addrinfo hints, *server;
+  int port_no;
+  int sockid;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage : %s port_no\n", argv[0]);
+    exit(1);
+  }
+
+  // TODO Initializer vs memset
+  // TODO Should macro?
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  int ret;
+  if ((ret = getaddrinfo(NULL, argv[1], &hints, &server)) != 0) {
+    if (ret == EAI_SYSTEM)
+      perror("getaddrinfo(): ");
+    else
+      fprintf(stderr, "getaddrinfo: %s", gai_strerror(ret));
+    exit(2);
+  }
+
+  // TODO Create loop for ipv6
+  if ((sockid = socket(server->ai_family, server->ai_socktype,
+                       server->ai_protocol)) == -1) {
+    perror("socket(): ");
+    exit(3);
+  }
+
+  if (bind(sockid, server->ai_addr, server->ai_addrlen) == -1) {
+    perror("bind(): ");
+    exit(4);
+  }
+
+  freeaddrinfo(server);
+
+  if (listen(sockid, BACKLOG) == -1) {
+    perror("listen(): ");
+    exit(5);
+  }
+
+  int client;
+  struct sockaddr_storage client_addr;
+  socklen_t addrlen = sizeof client_addr;
+
+  while (1) {
+    client = accept(sockid, (struct sockaddr *)&client_addr, &addrlen);
+    if (client == -1) {
+      perror("accept(): ");
+      exit(6);
+    }
+
+    printf("Connection : %s\n", "Someone");
+    close(client);
+  }
+  return 0;
+}
